3860ed1c68f06a76a8c9acc60c82e08d792838e7
v-turin <vladimir.turin@opencascade.com>
2018-11-08 15:00:20 +0300

    Linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,33 +11,32 @@ if [ "$cuda_impl" == "cuda" ]; then
     export CXXFLAGS="$CXXFLAGS -std=c++11"
 fi
 
-cmake -G "Ninja" \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DWITH_CUDA=$CUDA \
-  -DWITH_DAVIDSDK=OFF \
-  -DWITH_DSSDK=OFF \
-  -DWITH_ENSENSO=OFF \
-  -DWITH_FZAPI=OFF \
-  -DWITH_LIBUSB=OFF \
-  -DWITH_OPENGL=ON \
-  -DWITH_OPENNI=OFF \
-  -DWITH_OPENNI2=OFF \
-  -DWITH_PCAP=OFF \
-  -DWITH_PNG=ON \
-  -DWITH_QHULL=OFF \
-  -DWITH_QT=ON \
-  -DWITH_VTK=OFF \
-  -DBUILD_global_tests=OFF \
-  -DBUILD_examples=OFF \
-  -DBUILD_tools=ON \
-  -DBUILD_apps=OFF \
-  -DOPENGL_egl_LIBRARY=/usr/lib/x86_64-linux-gnu/libEGL.so \
-  -DOPENGL_glu_LIBRARY=/usr/lib/x86_64-linux-gnu/libGLU.so \
-  -DOPENGL_glx_LIBRARY=/usr/lib/x86_64-linux-gnu/libGLX.so \
-  -DOPENGL_opengl_LIBRARY=/usr/lib/x86_64-linux-gnu/libOpenGL.so \
-  -DOPENGL_gl_LIBRARY=/usr/lib/x86_64-linux-gnu/libGL.so \
-  -DOpenGL_GL_PREFERENCE=GLVND \
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig
+
+cmake -G "Ninja"                                \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX                \
+  -DCMAKE_PREFIX_PATH=$PREFIX                   \
+  -DCMAKE_BUILD_TYPE=Release                    \
+  -DWITH_CUDA=$CUDA                             \
+  -DWITH_DAVIDSDK=OFF                           \
+  -DWITH_DSSDK=OFF                              \
+  -DWITH_ENSENSO=OFF                            \
+  -DWITH_FZAPI=OFF                              \
+  -DWITH_LIBUSB=OFF                             \
+  -DWITH_OPENGL=ON                              \
+  -DWITH_OPENNI=OFF                             \
+  -DWITH_OPENNI2=OFF                            \
+  -DWITH_PCAP=OFF                               \
+  -DWITH_PNG=ON                                 \
+  -DWITH_QHULL=OFF                              \
+  -DWITH_QT=ON                                  \
+  -DWITH_VTK=OFF                                \
+  -DBUILD_global_tests=OFF                      \
+  -DBUILD_examples=OFF                          \
+  -DBUILD_tools=ON                              \
+  -DBUILD_apps=OFF                              \
+  -DCMAKE_LIBRARY_ARCHITECTURE=x86_64-linux-gnu \
+  -DOpenGL_GL_PREFERENCE=GLVND                  \
   ..
 
 ninja install
